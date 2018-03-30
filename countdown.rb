d68189e2299402e2ffb1dc083477a412ded80242
Chris Dundon <chris.dundon@gmail.com>
2018-03-30 13:46:52 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -9,6 +9,11 @@ def countdown(secs)
 end
 
 def countdown_with_sleep(secs)
+  while (secs > 0) do
+    puts "#{secs} SECOND(S)!"
+    Time.sleep(1)
+    secs -= 1
+  end
   
-  
+  "HAPPY NEW YEAR!"
 end
